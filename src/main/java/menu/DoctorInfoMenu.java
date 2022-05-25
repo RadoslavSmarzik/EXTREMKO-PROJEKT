cb9d63cd7e723b7eea22996d58f1685585950b58
Rado <radoslavsmaro@gmail.com>
2022-05-25 05:47:01 +0200

    Testy - DiseaseMenu

--- src/main/java/menu/DoctorInfoMenu.java
+++ src/main/java/menu/DoctorInfoMenu.java
@@ -25,7 +25,7 @@ public class DoctorInfoMenu extends Menu{
     @Override
     public void handle(String option) throws SQLException {
         if(loggedUser == null){
-            System.out.println("NOBODY IS LOGGED");
+            System.out.println("Nikto nie je prihlaseny.");
             return;
         }
         if(Integer.parseInt(option) == 1){
